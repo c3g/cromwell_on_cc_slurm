f037c59dcbf2e4ce5b7738c2416fdfe470a3f67b
P-O Quirion <pioliqui@gmail.com>
2020-09-09 16:00:54 -0400

    Add call cahing and enable restarts of jobs

---
+++ cw_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH -n 1 
+#SBATCH --time 1-00:00:00
+#SBATCH --mem-per-cpu 4775
+#SBATCH --job-name c3g_cromwell
+#SBATCH --chdir /project/rrg-bourqueg-ad/C3G/projects/CROMWELL_SERVER
+
+java -Dconfig.file=/home/poq/c3g/cromwell_on_cc_slurm/cc_cromwell.conf -jar /home/poq/CROMWELL/cromwell-52.jar   server
