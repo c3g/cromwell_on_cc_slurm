1ea71c7d0fa6575577549c7fbe82463429019c12
P-O Quirion <pioliqui@gmail.com>
2020-09-15 15:47:21 -0400

    load explicitely the c3g software stack

--- cw_server.sh
+++ cw_server.sh
@@ -6,6 +6,10 @@
 #SBATCH --job-name c3g_cromwell
 
 module load java/13
+# load c3g software stack
+export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
+module use $MUGQIC_INSTALL_HOME/modulefiles
+
 
 # Path of the script
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -19,8 +23,6 @@ SLURM_TMPDIR=${SLURM_TMPDIR:-/tmp}
 mkdir -p ${SLURM_TMPDIR}/cromwell-executions/cromwell-db
 
 rsync -rv ./cromwell-executions/cromwell-db/ ${SLURM_TMPDIR}/cromwell-executions/cromwell-db/
-
-source ~/init/genpipes.sh
 
 sed "s@__DATA_BASE_ROOT__@$SLURM_TMPDIR/@g"  ${script_path}/cc_cromwell.conf.tpl > ${SLURM_TMPDIR}/cc_cromwell.conf
 
